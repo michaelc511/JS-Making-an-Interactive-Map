64c35f52a36250f7420d20887ae4c94651474800
michaelc511 <michaelc511@gmail.com>
2023-06-05 15:47:20 -0700

    added more comments

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-// P2 map object
+/*
 
-// P2 get coordinates via geolocation api
+	Please follow the order for Parts 2 and 3.
 
-// P3 get foursquare businesses
+	P2.1 = first item for part 2
+	P2.2 = 2nd item for part 2
+	....
 
-// P3 process foursquare array
+	Talk about the variables and functions needed for the map object
+
+	For Part 3, review the FourSquare documentationand follow the order 
+
+	P3.1
+	P3.2
+	P3.3...
+*/
+
+// P2.2 map object
+	/*
+		coordincates array
+		businesses array
+
+		map {}
+		markers{}
+
+		buildMap() function
+
+		addMarkers() function
+	*/
+
+// P2.1 get coordinates via geolocation api
+
+// P3.2 get foursquare businesses
+
+// P3.3 process foursquare array
 
 // P2 event handlers
-// P2 window load
+// P2.1 window load
 
-// P2 business submit button
+// P3.1 business submit button
 
